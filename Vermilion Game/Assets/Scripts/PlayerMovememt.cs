d89f91172cec78e4318a2f1293e6fd8a14b95adc
Animeshark <milo.hudsonquirke@gmail.com>
2025-02-01 12:41:48 +0000

    Movement
    sdafasdf

--- Vermilion Game/Assets/Scripts/PlayerMovememt.cs
+++ Vermilion Game/Assets/Scripts/PlayerMovememt.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerMovememt : MonoBehaviour
 {
+
+    float walkSpeed = 1f;
+    float sprintMulti = 3f;
+    bool isSprinting = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,36 @@ public class PlayerMovememt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Sprint();
+        Movement();
+
+    }
+
+    private void Movement()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
         
+
+        if (isSprinting)
+        {
+            transform.position = new Vector2(transform.position.x + horizontal * walkSpeed * sprintMulti, transform.position.y + vertical * walkSpeed * sprintMulti);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x + horizontal * walkSpeed, transform.position.y + vertical * walkSpeed);
+        }
+    }
+
+    private void Sprint()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isSprinting = true;
+        }
+        else
+        {
+            isSprinting = false;
+        }
     }
 }
